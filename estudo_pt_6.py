d410458086a6e21534bec809f34e8cbd121888b8
kkk_gui <157399473+fvgbffvvvd@users.noreply.github.com>
2025-01-16 20:48:04 -0300

    Update estudo_pt_6.py

--- estudo_pt_6.py
+++ estudo_pt_6.py
@@ -1,9 +1,27 @@
-valor = float(input("qual o valor da casa? :"))
-salario = float(input("qual seu salario ? :"))
-anos = int(input("quantos anos voce deseja pagar essa casa ? :"))
-prestacao_mensal = valor /( anos * 12)
+# valor = float(input("qual o valor da casa? :"))
+# salario = float(input("qual seu salario ? :"))
+# anos = int(input("quantos anos voce deseja pagar essa casa ? :"))
+# prestacao_mensal = valor /( anos * 12)
 
-if prestacao_mensal > salario * 0.30 :
-    print("nao podemos fazer essa ação")
-else:
-    print(" o emprestimo foi aceito ") 
+# if prestacao_mensal > salario * 0.30 :
+#     print("nao podemos fazer essa ação")
+# else:
+#     print(" o emprestimo foi aceito ") 
+
+
+# objetivo = pergunte a quantidade de kwh consumida e o tipo de instalação, sendo R residencia, I para industria e C para comercio.
+# R  <= 500; C <=1000; I <=5000
+
+
+# kwh = int(input("digite a quantidade de kwh gasta no mês :"))
+# instalacao = (input("a sua instalação, é Residencial, industrial ou comercial? :"))
+# inst = instalacao
+
+# if kwh <= 500:
+#     print("{}".format(kwh * 0.40)) #ate 500
+# elif (kwh > 500) and (kwh <= 1000):# acima de 500
+#     print("{}".format(kwh * 0.65))
+# elif (kwh > 1000) and (kwh < 5000 ):#maior que 1000
+#     print("{}".format(kwh * 0.55))
+# elif kwh > 5000:
+#     print("{}".format(kwh * 0.60))
